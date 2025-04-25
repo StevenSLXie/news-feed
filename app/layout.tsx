4a718616ccbe206d9114a481ce58e4d510e739e2
Steven S.L. Xie <stevenslxie@gmail.com>
2025-04-25 11:31:51 +0800

    1.0.22 tailwind bug-fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import NextAuthSessionProvider from "./SessionProvider";
-import '../styles/globals.css'
+import '../app/globals.css'
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
